02771c6b677ec627270720b1eb10ba75a2a3e2fd
leonnig <jason901214edaaa@gmail.com>
2025-03-10 17:05:45 +0800

    Implement free
    This function traverses each element and release it in order.
    
    Change-Id: Id958ddcba1af26f13cb007fac26ad65b2b06bb3a

--- queue.c
+++ queue.c
@@ -17,7 +17,19 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (head) {
+        struct list_head *cur = head->next;
+        while (cur != head) {
+            element_t *ele;
+            ele = container_of(cur, element_t, list);
+            cur = cur->next;
+            q_release_element(ele);
+        }
+        free(head);
+    }
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
